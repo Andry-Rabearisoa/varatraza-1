b5cf538c89316b036becebc15aadc558fe687732
Carnot <mireillamireilla9@gmail.com>
2025-07-24 14:46:00 +0300

    x

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar.component';  // Importer ici
-
+import { FooterComponent } from './shared/footer/footer.component';  // Importer ici
 @Component({
   selector: 'app-root',
   standalone: true,  // Probablement déjà en standalone
-  imports: [RouterOutlet, NavbarComponent],  // Ajouter NavbarComponent ici
+  imports: [RouterOutlet, NavbarComponent,FooterComponent],  // Ajouter NavbarComponent ici
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
