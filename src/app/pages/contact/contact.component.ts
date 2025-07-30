66b5d9eb3906a21629f0ef721a9923fa0fdaf97d
Carnot <mireillamireilla9@gmail.com>
2025-07-30 15:25:46 +0300

    x

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -8,48 +8,10 @@ import { CommonModule } from '@angular/common';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent implements AfterViewInit, OnDestroy {
+export class ContactComponent {
   images: string[] = [];
 
-  @ViewChild('carousel', { static: false }) carousel!: ElementRef<HTMLDivElement>;
-
-  autoScrollInterval: any;
-
   constructor() {
-    // Charge les images : c1.jpg à c12.jpg
     this.images = Array.from({ length: 12 }, (_, i) => `assets/c${i + 1}.jpg`);
-  }
-
-  ngAfterViewInit() {
-    this.startAutoScroll();
-  }
-
-  ngOnDestroy() {
-    clearInterval(this.autoScrollInterval);
-  }
-
-  startAutoScroll() {
-    this.autoScrollInterval = setInterval(() => {
-      this.scrollRight();
-    }, 3000); // toutes les 3 secondes
-  }
-
-  scrollLeft() {
-    this.resetAutoScroll();
-    const carouselEl = this.carousel.nativeElement;
-    const scrollAmount = carouselEl.clientWidth / 4; // largeur 1 image (4 images visibles)
-    carouselEl.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-  }
-
-  scrollRight() {
-    this.resetAutoScroll();
-    const carouselEl = this.carousel.nativeElement;
-    const scrollAmount = carouselEl.clientWidth / 4; // largeur 1 image
-    carouselEl.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-  }
-
-  resetAutoScroll() {
-    clearInterval(this.autoScrollInterval);
-    this.startAutoScroll();
   }
 }
