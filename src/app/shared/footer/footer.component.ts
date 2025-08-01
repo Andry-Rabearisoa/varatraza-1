178b694368409cbef6afdde4a6cc8e89bca2db74
Carnot <mireillamireilla9@gmail.com>
2025-08-01 10:06:01 +0300

    x

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -2,10 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
 
+  onFaireUnDon(): void {
+    // 👇 Tu peux ici ajouter un log, analytics, animation, etc.
+    console.log('Bouton "Faire un don" cliqué');
+
+    // Ouvrir le lien dans un nouvel onglet
+    window.open('https://forms.gle/XG1KyFSECpKMPYzu6', '_blank');
+  }
+  onDevenirBenevole(): void {
+    console.log('Bouton "Devenir bénévole" cliqué');
+    window.open('https://forms.gle/AexZnHCxmN4G2CCZ9', '_blank');
+  }
+  
 }
