91d68309689bfa3aa7c3debc1d16bbedd50ee319
Andry-Rabearisoa <andryarisoaramary@gmail.com>
2025-08-19 07:10:28 +0300

    Update nos-realisations.component.ts

--- src/app/pages/nos-realisations/nos-realisations.component.ts
+++ src/app/pages/nos-realisations/nos-realisations.component.ts
@@ -18,19 +18,13 @@ export class NosRealisationsComponent {
       description: `Équipement d’éclairage public : distribution de plus de 200 lampadaires solaires au niveau des Fokontany dans le district de Mitsinjo.<br>Période : d’avril 2024 à mars 2025<br>Coût total des lampadaires distribués : 40 000 000 ariary, soit 8 333 euros.`,
       image: 'image2RE.jpg'
     },
-    {
-      description: `Réhabilitation et extension du lycée d’enseignement général de Mitsinjo, en partenariat avec la Fondation AXIAN.<br>Début des travaux : août 2024<br>Achèvement : octobre 2024<br>Inauguration : novembre 2024<br>Coût des travaux : 650 000 ariary, soit 135 417 euros.`,
-      image: 'image3RE.jpg'
-    },
+    
     {
       description: `Distribution de matériels, d’effets et d’équipements sportifs aux jeunes auprès des Fokontany dans le district de Mitsinjo.<br>Période : de mars 2024 à décembre 2024<br>Coût : 15 000 000 ariary, soit 3 125 euros.`,
       image: 'image4RE.jpg'
     },
 
-    {
-      description: `Aide financière d’un montant de 3 500 000 ariary par fokontany dans le District de Mitsinjo.<br>Bénéficiaires : 58 Fokontany<br>Motifs : financement de diverses activités axées sur le développement au sein des fokontany.<br>Coût de l’aide en totalité : 28 000 000 ariary, soit 5 600 euros.`,
-      image: '1753430901216.jpg'
-    },
+
     {
       description: `Don de pirogues métalliques pour soutenir les communautés de Mitsinjo<br>
       En novembre 2024, l’Association Varatraza, en collaboration avec le Député du district de Mitsinjo, a offert deux pirogues métalliques aux communautés locales. Cette initiative vise à renforcer l’accès aux voies navigables, faciliter les déplacements et soutenir les activités économiques des populations riveraines, contribuant ainsi au développement durable de Mitsinjo.<br>
